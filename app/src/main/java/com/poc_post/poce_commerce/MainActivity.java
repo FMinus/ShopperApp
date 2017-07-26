b23695bccc95d1256864823c67c39ee56edfaecc
Ayoub DEQQAQ <ayoub.deqqaq@ma.ibm.com>
2017-07-26 12:08:33 +0100

    removed viewpager

--- app/src/main/java/com/poc_post/poce_commerce/MainActivity.java
+++ app/src/main/java/com/poc_post/poce_commerce/MainActivity.java
@@ -18,8 +18,6 @@ import butterknife.BindView;
 
 public class MainActivity extends BaseActivity {
 
-//    @BindView(R.id.orders_viewPager) protected ViewPager viewPager;
-//    @BindView(R.id.orders_tabLayout) protected TabLayout tabLayout;
 
     @Override
     public int getContentViewLayout() {
@@ -37,34 +35,7 @@ public class MainActivity extends BaseActivity {
         if (savedInstanceState == null) {
             showFragment(ProductListFragment.newInstance());
         }
-//        setupViewPagerLayout();
     }
-
-//    private void setupViewPagerLayout() {
-//        viewPager.setAdapter(createViewPagerAdapter());
-//        tabLayout.setupWithViewPager(viewPager);
-//        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//            }
-//        });
-//        setAppBarTitle("Shopping app");
-//    }
-//
-//    private ViewPagerAdapter createViewPagerAdapter() {
-//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-//        adapter.addFragment("Products List", ProductListFragment.newInstance());
-//        return adapter;
-//    }
 
     public void showFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
